64bf3a2e8b905311b766e481f09ecd9fa9618b0f
Cedrick Embestro <90442361+Cedrick13@users.noreply.github.com>
2023-06-10 14:10:59 +0800

    Add files via upload

---
+++ homepage.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="en">
+<head> 
+<meta charset="UTF-8"> 
+<title>Website</title> 
+<link rel="stylesheet" href="style.css"> 
+ 
+ <body> 
+ <div class="main"> 
+      <nav> 
+          <ul> 
+              <li><a href="#">Home</a></li> 
+              <li><a href="#">About Me</a></li> 
+              <form action="index.php" method="post">
+                <button type="submit" href="index.php" class="btn btn-danger" id="logout" name="logout">Logout</button>
+            </form>
+          </ul> 
+          <div class="logo"> 
+              <h1>Portfolio</h1> 
+          </div> 
+      </nav> 
+  </div> 
+  <div class="text"> 
+      <h2> Hey My Name Is<br> <strong>Cedrick</strong> 
+   <br>And I'm A <br><strong>Web Designer </strong></h2> 
+  </div> 
+  <div class="btn">
+      <a href="#">Contact Me</a>
+  </div> 
+ </body>
+</html>
